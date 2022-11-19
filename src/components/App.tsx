d3b6464b2fe4251dc6b3f24e01133b01fe45088e
N2-Met <yuyushiki11@gmail.com>
2022-11-19 10:41:36 +0900

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,75 @@
-export const App = () => {
-  return <h1>メモアプリ</h1>;
+import { ChangeEvent, useState, FC } from "react";
+import styled from "styled-components";
+
+
+
+export const App: FC = () => {
+
+  //テキストボックスState
+  const [text, setText] = useState<string>("");
+
+  //メモ一覧State
+  const [memos, setMemos] = useState<string[]>([]);
+
+  //テキストボックス入力時に入力内容をStateに反映
+  //__反映先はテキストボックスのState
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)
+
+  //「追加」ボタン押下時
+  const onClickAdd = () => {
+    //State変更を正常に検知されるために新たな配列尾を生成
+    const newMemos = [...memos];
+    //テキストボックスの入力内容をメモ配列に追加
+    newMemos.push(text);
+    setMemos(newMemos);
+    //テキストボックスを空に
+    setText("");
+  };
+
+  //「削除」ボタン押下時(何番目が押されたか引数で受け取る)
+  const onClickDelete = (index: number) => {
+    //State変更を正常に検知させるため新たな配列を作成
+    const newMemos = [...memos];
+    //メモ配列から外套の要素を削除
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
+  return (
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
+    </div>
+  );
 };
+
+
+//Style
+const SButton = styled.button`margin-left:16px`;
+const SContainer = styled.div`
+  border:solid 1px #ccc;
+  padding:16px;
+  margin:8px;
+`;
+const SMemoWrapper = styled.div`
+  display:flex;
+  aligin-items:center;
+`;
+
 
 
 
